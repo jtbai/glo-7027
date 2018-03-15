68410a8e40c8bf1d5453d3b3e66222f11a5bac68
Christopher Blier-Wong <christopher.blierwong@gmail.com>
2018-03-15 11:18:27 -0400

    now is work hao

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from config_classifier import ConfigClassifier
 from sklearn import model_selection
-from json import load
+import pickle
+import json
 import time
 
-config_dictionary = load(open("initial_run_test.json"))
+config_dictionary = json.load(open("initial_run_test.json"))
 Config = ConfigClassifier(config_dictionary)
 
 
@@ -11,7 +12,7 @@ Config = ConfigClassifier(config_dictionary)
 if __name__ == '__main__':
 
 
-    train = load(open('./prepared_data.pyk', 'rb'))
+    train = pickle.load(open('./prepared_data.pyk', 'rb'))
 
     X = train.loc[:, train.columns != "SalePrice"]
     # X = train.loc[:, train.columns[[range(1, 6)]]]
@@ -20,5 +21,5 @@ if __name__ == '__main__':
     X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.3)
 
     for model_to_execute in Config.get_jobs():
-        model_to_execute.execute(X_train, X_test, y_train, y_test)
+        model_to_execute.execute(X_train, y_train, X_test, y_test)
 
