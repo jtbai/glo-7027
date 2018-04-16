51a61bc6843114446f895894e7647e21612cccb3
Jean-Thomas Baillargeon <jtbaillargeon@hotmail.com>
2018-04-15 20:39:36 -0400

    ajout polynomaial svm

--- models/__init__.py
+++ models/__init__.py
@@ -2,3 +2,4 @@ from models.gradient_boosting_regression import GradientBoostingRegression
 from models.random_forest_regression import RandomForestRegression
 from models.simple_linear_regression import SimpleLinearRegression
 from models.support_vector_machine_regression import SupportVectorMachineRegression
+from models.support_vector_machine_regression_poly3 import  SupportVectorMachineRegressionPoly3
---
+++ models/support_vector_machine_regression_poly3.py
@@ -0,0 +1,14 @@
+from models.basemodel import BaseModel
+from sklearn import model_selection, svm
+
+
+class SupportVectorMachineRegressionPoly3(BaseModel):
+
+    def _train(self, X_train, y_train):
+        parametres = {'gamma': [0.01, 0.1, 1], 'C': [1, 10, 100], 'degree': [2,3,4,5,6]}
+        grid_search = model_selection.GridSearchCV(svm.SVR(kernel="poly" ), parametres, n_jobs=6)
+        grid_search = grid_search.fit(X_train, y_train)
+        return grid_search
+
+    def compute_and_output_r2_metric(self, trained_grid_search, y_train, y_train_pred, y_test, y_test_pred):
+        self._printResults(y_train, y_train_pred, y_test, y_test_pred, str(trained_grid_search.best_params_))
