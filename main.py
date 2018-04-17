cf3e3f8af63e3195c14824d291c5729990937b3e
Jean-Thomas Baillargeon <jtbaillargeon@hotmail.com>
2018-04-17 12:08:34 -0400

    update main

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ CONFIG_PATH = "config"
 DATA_PATH = "data"
 
 input_data_file_name = 'train_prepared_data.pyk'
-config_file_name = "initial_run.json"
+config_file_name = "test_boostin_pca_20.json"
 
 config_dictionary = json.load(open(path.join(CONFIG_PATH, config_file_name)))
 Config = ClassifierConfigurationFactory(config_dictionary)
@@ -27,24 +27,8 @@ if __name__ == '__main__':
     folds = 10
     k_fold = model_selection.KFold(n_splits=folds)
 
-    for index, k_fold_indexes in enumerate(k_fold.split(X)):
-        fold_start_time = time.time()
+    transformed_train_x = Transformation.fit_transform(X)
 
-        X_train = X.iloc[k_fold_indexes[0]]
-        y_train = y.iloc[k_fold_indexes[0]]
-        X_test = X.iloc[k_fold_indexes[1]]
-        y_test = y.iloc[k_fold_indexes[1]]
+    for model_to_execute in Config.get_jobs():
+        model_to_execute.execute(transformed_train_x, y, None, None)
 
-        transformed_train_x = Transformation.fit_transform(X_train)
-        transformed_test_x = Transformation.fit_transform(X_test)
-
-        for model_to_execute in Config.get_jobs():
-            model_to_execute.execute(X_train, y_train, X_test, y_test)
-
-        fold_finish_time = time.time()
-        fold_time = fold_finish_time - fold_start_time
-        total_time = fold_finish_time - start_time
-        time_record = "fold {}/{} : {}s (total: {})".format(index+1, len(X), fold_time, total_time)
-        print(time_record)
-        with open("time_record.txt",'a') as time_file:
-            time_file.write("{}\n".format(time_record))
